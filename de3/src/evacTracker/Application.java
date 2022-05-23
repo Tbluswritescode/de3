1d8d19de51f4ed65bbc00be02a24ca71fbb7ad63
Tristan Blus <tgblus@knox.edu>
2022-05-22 22:16:11 -0500

    filling out getters, contrustors, and toString methods for person and application

--- de3/src/evacTracker/Application.java
+++ de3/src/evacTracker/Application.java
@@ -1,5 +1,35 @@
 package evacTracker;
 
-public class Application {
+import java.util.Date;
 
+public class Application {
+    private Date applicationDate;
+    private Person person;
+
+    Application() {
+        applicationDate = new Date(System.currentTimeMillis());
+    }
+
+    Application(Person p) {
+        person = p;
+        applicationDate = new Date(System.currentTimeMillis());
+    }
+
+    public Date getDate() {
+        return applicationDate;
+    }
+
+    public String getAddress() {
+        return person.getAddress();
+    }
+
+    public void setPerson(Person p) {
+        person = p;
+    }
+
+    public String toString() {
+        String rs = "";
+        rs += "Applied on " + applicationDate + ": " + person;
+        return rs;
+    }
 }
--- de3/src/evacTracker/Person.java
+++ de3/src/evacTracker/Person.java
@@ -3,5 +3,25 @@ package evacTracker;
 public class Person {
 
     private String name;
-    private Application app;
+    private String address;
+
+    Person(String n, String a) {
+        name = n;
+        address = a;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        String rs = "";
+        rs += name + ": " + address + "\n";
+        return rs;
+    }
+
 }
